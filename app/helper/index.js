8bbf60edd10a3f23033f91e1aa3d056a6f4db1ee
Vikesh Tiwari <tvicky002@gmail.com>
2017-10-11 18:35:31 +0530

    helper functions and schema

---
+++ app/helper/index.js
@@ -0,0 +1,4 @@
+/**
+ * This file should contain all the helper functions that are needed in order to
+ * do any kind of custom operation that might be needed throughout our project
+ */
---
+++ app/models/index.js
@@ -0,0 +1,5 @@
+/**
+ * This file should import all the schema and register a mongoose model
+ * here and export the same. This is the file which should be imported during
+ * any of the database operation that needs to be done throughout our project
+ */
