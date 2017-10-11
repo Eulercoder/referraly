f7c1188f61e282294174a55ec29cb362d32a9ec4
Vikesh Tiwari <tvicky002@gmail.com>
2017-10-11 18:30:31 +0530

    contains all the routes

---
+++ app/controllers/admin/index.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * This file should export all the subroutes under the entity to which
+ * this folder belongs to. It must be ensured that the folder name matches
+ * the entity name and the subroute to which it is related to.
+ *
+ * For example: this file is only going to export all the sub-routes under
+ * /codegenerate/api/v1/admin
+ *
+ */
